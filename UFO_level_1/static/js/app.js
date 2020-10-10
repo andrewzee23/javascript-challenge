84243893d49024150e2e4a676fd799901a914d48
andrewzee23 <andrewazamora@gmail.com>
2020-10-10 09:47:31 -0500

    modified app.js

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -1,4 +1,28 @@
 // from data.js
 let tableData = data;
 
-// YOUR CODE HERE!
+// select the button
+let button = d3.select('#filter-btn');
+
+// select the form
+let form = d3.select('#form');
+
+// create event handlers
+button.on('click', runEnter);
+form.on('submit', runEnter);
+
+// complete the event handler function for the form
+function runEnter() {
+    d3.event.preventDefault();
+
+    let inputElement = d3.select('#datetime');
+
+    let inputValue = inputElement.property('value');
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    let filteredData = tableData.filter(data => data.datetime === inputValue);
+
+    console.log(filteredData);
+}
