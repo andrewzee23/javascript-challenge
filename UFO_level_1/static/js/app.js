44f00b24f550b75a0cdbd3b0e6e74db123e7f855
andrewzee23 <andrewazamora@gmail.com>
2020-10-11 09:18:45 -0500

    modified app.js

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -1,28 +1,48 @@
-// from data.js
 let tableData = data;
 
-// select the button
+let tbody =d3.select('tbody');
+
+tableData.forEach(function (ufosightings) {
+    console.log(ufosightings);
+
+    let row = tbody.append('tr');
+
+    Object.entries(ufosightings).forEach(function([key, value]) {
+        console.log(key, value);
+
+        let cell = row.append('td');
+        cell.text(value);
+    });
+});
+
 let button = d3.select('#filter-btn');
 
-// select the form
-let form = d3.select('#form');
-
-// create event handlers
-button.on('click', runEnter);
-form.on('submit', runEnter);
-
-// complete the event handler function for the form
 function runEnter() {
-    d3.event.preventDefault();
+    tbody.html('');
 
     let inputElement = d3.select('#datetime');
 
     let inputValue = inputElement.property('value');
 
     console.log(inputValue);
-    console.log(tableData);
 
-    let filteredData = tableData.filter(data => data.datetime === inputValue);
+    let filteredData = tableData.filter(sightings => sightings.datetime === inputValue);
 
     console.log(filteredData);
-}
+
+    filteredData.forEach(function(results) {
+        console.log(results);
+
+        let row = tbody.append('tr');
+
+        Object.entries(results).forEach(function([key, value]) {
+            console.log(key, value);
+    
+            let cell = row.append('td');
+            cell.text(value);
+        });
+
+    });
+};
+
+button.on('click', runEnter);
